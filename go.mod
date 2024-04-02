f45c0714e7a9e26be0795fee40d00b294be60a93
Kirill <krill.pestun@gmail.com>
2024-04-02 15:00:17 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/Kiruhanchik/TestovoyeGo
+
+go 1.22.1
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/ClickHouse/clickhouse-go v1.5.4 // indirect
+	github.com/cloudflare/golz4 v0.0.0-20150217214814-ef862a3cdc58 // indirect
+	github.com/denisenkom/go-mssqldb v0.12.3 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe // indirect
+	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pressly/goose v2.7.0+incompatible // indirect
+	github.com/ziutek/mymysql v1.5.4 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+)
---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Car struct {
+	Id     int    `json:"Id"`
+	RegNum string `json:"regNum"`
+	Mark   string `json:"mark"`
+	Model  string `json:"model"`
+	Year   int    `json:"year"`
+	Owner  People `json:"owner"`
+}
+
+type People struct {
+	Id         int    `json:"Id"`
+	Name       string `json:"name"`
+	Surname    string `json:"surname"`
+	Patronymic string `json:"patronymic"`
+}
+
+var cars []Car
+
+func getCars(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(cars)
+}
+
+func deleteCar(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r) // Получаем параметры из URL, в данном случае - ID автомобиля
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	for index := range cars {
+		if index == id {
+			cars = append(cars[:index], cars[index+1:]...)
+			w.WriteHeader(http.StatusNoContent) // Возвращаем статус 204 No Content в случае успешного удаления
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusNotFound) // Возвращаем статус 404 Not Found, если автомобиль не найден
+}
+
+func updateCar(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid car ID", http.StatusBadRequest)
+		return
+	}
+
+	var updatedCar Car
+	err = json.NewDecoder(r.Body).Decode(&updatedCar)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	if id < 0 || id >= len(cars) {
+		http.Error(w, "Car ID not found", http.StatusNotFound)
+		return
+	}
+
+	cars[id] = updatedCar
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(cars[id])
+}
+
+func addCar(w http.ResponseWriter, r *http.Request) {
+	var requestData struct {
+		RegNums []string `json:"regNums"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&requestData)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	for _, regNum := range requestData.RegNums {
+		newCar := Car{RegNum: regNum}
+		cars = append(cars, newCar)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(requestData)
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/cars", getCars).Methods("GET")
+	router.HandleFunc("/cars/{id}", deleteCar).Methods("DELETE")
+	router.HandleFunc("/cars/{id}", updateCar).Methods("PATCH")
+	router.HandleFunc("/cars", addCar).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
